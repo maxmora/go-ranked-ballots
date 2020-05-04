5a7bdbfc6684ed354ae8c86d00ee8c2f65f4c8eb
chris <chris@riekko>
2020-05-04 01:26:32 -0400

    put iterations in a loop, but there is a bug

--- instant_runoff.go
+++ instant_runoff.go
@@ -27,6 +27,17 @@ func countWinningVotes(voters []voter) (candidateToVoteCount map[string]uint) {
 }
 
 
+func countCandidates(voters []voter) (numCandidates int) {
+	allCandidates := make(map[string]bool)
+	for _, voter := range(voters) {
+		for _, candidate := range(voter.votes) {
+			allCandidates[candidate] = true
+		}
+	}
+	return len(allCandidates)
+}
+
+
 func sortIterationResults(candidateToVoteCount map[string]uint) (sortedResults []iterationResult) {
 	for candidate, voteCount := range(candidateToVoteCount) {
 		sortedResults = append(
@@ -52,71 +63,74 @@ func sortIterationResults(candidateToVoteCount map[string]uint) (sortedResults [
 
 
 func computeInstantRunoffWinner(voters []voter) (winningCandidates []string, err error) {
-	// TODO FIXME do the whole appropriate bit in a loop.
+	numCandidates := countCandidates(voters)
 
-	candidateToVoteCount := countWinningVotes(voters)
-	sortedResults := sortIterationResults(candidateToVoteCount)
+	// We should never need to do more iterations than there are candidates to consider.
+	for iterationNumber := 1; iterationNumber < numCandidates; iterationNumber++ {
+		candidateToVoteCount := countWinningVotes(voters)
+		sortedResults := sortIterationResults(candidateToVoteCount)
 
-	fmt.Println("Results of iteration 1:")
-	for _, sr := range(sortedResults) {
-		fmt.Printf("\t%s: %d\n", sr.candidate, sr.voteCount)
-	}
-
-	// Collect all candidate(s) with the most and least votes first choice votes.
-	minFirstChoiceVotes := sortedResults[0].voteCount
-	maxFirstChoiceVotes := sortedResults[len(sortedResults)-1].voteCount
-	var worstCandidates []iterationResult
-	var bestCandidates []iterationResult
-	for _, res := range(sortedResults) {
-		if res.voteCount == minFirstChoiceVotes {
-			worstCandidates = append(worstCandidates, res)
-		}
-		if res.voteCount == maxFirstChoiceVotes {
-			bestCandidates = append(bestCandidates, res)
-		}
-	}
-
-	fmt.Println("The winner(s) is/are:")
-	for _, sr := range(bestCandidates) {
-		fmt.Printf("\t%s: %d\n", sr.candidate, sr.voteCount)
-	}
-	if float64(maxFirstChoiceVotes) > float64(len(voters)) {
-		fmt.Printf("%d votes is a simple majority of %d voters, so we have (a) winner(s).\n", maxFirstChoiceVotes, len(voters))
-		for _, res := range(bestCandidates) {
-			winningCandidates = append(winningCandidates, res.candidate)
-		}
-		return winningCandidates, nil
-	} else {
-		fmt.Printf("%d votes is not a simple majority of %d voters; beginning next iteration.\n", maxFirstChoiceVotes, len(voters))
-		// TODO then we go to the next iteration.
-		// TODO pick one of the losers at random to eliminate, elimiate it from all
-		// voters for which it was the top candidate, and continue to the next iteration.
-		fmt.Println("The candidate(s) with the least first choice votes is/are:")
-		for _, sr := range(worstCandidates) {
+		fmt.Printf("First choice votes for iteration %d:\n", iterationNumber)
+		for _, sr := range(sortedResults) {
 			fmt.Printf("\t%s: %d\n", sr.candidate, sr.voteCount)
 		}
-		if len(worstCandidates) < 1 {
-			panic("No losing candidates. This doesn't make sense.")
-		}
-		losingCandidate := worstCandidates[rand.Intn(len(worstCandidates))].candidate
-		fmt.Printf("Randomly selected candidate to eliminate: %s\n", losingCandidate)
 
-		// Eliminate losing candidate.
-		for i, _ := range(voters) {
-			// Check if there are any candidates left to count from this voter.
-			if len(voters[i].votes) < 1 {
-				continue
+		// Collect all candidate(s) with the most and least votes first choice votes.
+		minFirstChoiceVotes := sortedResults[0].voteCount
+		maxFirstChoiceVotes := sortedResults[len(sortedResults)-1].voteCount
+		var worstCandidates []iterationResult
+		var bestCandidates []iterationResult
+		for _, res := range(sortedResults) {
+			if res.voteCount == minFirstChoiceVotes {
+				worstCandidates = append(worstCandidates, res)
 			}
+			if res.voteCount == maxFirstChoiceVotes {
+				bestCandidates = append(bestCandidates, res)
+			}
+		}
 
-			if voters[i].votes[0] == losingCandidate {
-				// We have to do this slice manipulation on voters[i] because that rewrites
-				// the actual in the slice. If we range over `_, v` and rewrite the loop
-				// variable's `v.votes`, it won't mutate the actual voter.
-				voters[i].votes = voters[i].votes[1:]
+		fmt.Println("The winner(s) is/are:")
+		for _, sr := range(bestCandidates) {
+			fmt.Printf("\t%s: %d\n", sr.candidate, sr.voteCount)
+		}
+		if float64(maxFirstChoiceVotes) > float64(len(voters)) {
+			fmt.Printf("%d votes is a simple majority of %d voters, so we have (a) winner(s).\n", maxFirstChoiceVotes, len(voters))
+			for _, res := range(bestCandidates) {
+				winningCandidates = append(winningCandidates, res.candidate)
+			}
+			return winningCandidates, nil
+		} else {
+			fmt.Printf("%d votes is not a simple majority of %d voters; beginning next iteration.\n", maxFirstChoiceVotes, len(voters))
+			// TODO then we go to the next iteration.
+			// TODO pick one of the losers at random to eliminate, elimiate it from all
+			// voters for which it was the top candidate, and continue to the next iteration.
+			fmt.Println("The candidate(s) with the least first choice votes is/are:")
+			for _, sr := range(worstCandidates) {
+				fmt.Printf("\t%s: %d\n", sr.candidate, sr.voteCount)
+			}
+			if len(worstCandidates) < 1 {
+				panic("No losing candidates. This doesn't make sense.")
+			}
+			losingCandidate := worstCandidates[rand.Intn(len(worstCandidates))].candidate
+			fmt.Printf("Randomly selected candidate to eliminate: %s\n", losingCandidate)
+
+			// Eliminate losing candidate.
+			for i, _ := range(voters) {
+				// Check if there are any candidates left to count from this voter.
+				if len(voters[i].votes) < 1 {
+					continue
+				}
+
+				if voters[i].votes[0] == losingCandidate {
+					// We have to do this slice manipulation on voters[i] because that rewrites
+					// the actual in the slice. If we range over `_, v` and rewrite the loop
+					// variable's `v.votes`, it won't mutate the actual voter.
+					voters[i].votes = voters[i].votes[1:]
+				}
 			}
 		}
 	}
 
-	return []string{}, fmt.Errorf("Something went wrong.")
+	return []string{}, fmt.Errorf("Something went wrong; we never determined winners!")
 }
 
